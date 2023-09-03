15b55bb4cc0087298377df850a24d558e4e9a80e
Rohan-kolewad7 <132587323+Rohan-kolewad7@users.noreply.github.com>
2023-09-03 20:23:27 +0530

    Update Dash2.py

--- dash/Dash2.py
+++ dash/Dash2.py
@@ -31,9 +31,11 @@ g_drive_CWC_agency = "https://docs.google.com/spreadsheets/d/1NK3pVebnRxcKzmtAn-
 
 
 # Load CSV data into DataFrames
-df_rainfall_state = pd.read_csv(g_drive_rainfall,chunksize=100)
-#df_rainfall_state['Date'] = pd.to_datetime(df_rainfall_state['Date'])
+df_rainfall_state = pd.DataFrame(pd.read_csv(g_drive_rainfall,chunksize=100))
 df_rainfall_state['Date'] = pd.to_datetime(df_rainfall_state['Date'], format='%d-%m-%Y')
+
+#df_rainfall_state['Date'] = pd.to_datetime(df_rainfall_state['Date'])
+
 
 
 
