a0d82410c016ceb5946772507a9c02240d01c299
Rohan-kolewad7 <132587323+Rohan-kolewad7@users.noreply.github.com>
2023-09-03 20:08:45 +0530

    Update Dash2.py

--- dash/Dash2.py
+++ dash/Dash2.py
@@ -7,21 +7,45 @@ import seaborn as sns
 
 st.title("Water in India: A Dashboard of Information")
 
-rainfall_state_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned.csv'
-cleaned_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_Sub-basin-wise_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned.csv'
-surface_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/SW_CPCP_and_CWC_Cleaned.csv'
-reservoirs_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_data_of_reservoir_level_of_CWC_Agency_2000_2023.csv'
+# rainfall_state_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned.csv'
+# cleaned_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_Sub-basin-wise_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned.csv'
+# surface_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/SW_CPCP_and_CWC_Cleaned.csv'
+# reservoirs_url = 'https://raw.githubusercontent.com/Rohan-kolewad7/Dasboard/main/dash/Daily_data_of_reservoir_level_of_CWC_Agency_2000_2023.csv'
+
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------
+## using G-drive for load Csv into dataframes
+
+# Get the shareable link to the Google Drive file
+
+# Daily_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned
+g_drive_rainfall = "https://docs.google.com/spreadsheets/d/1XAdakIiH5xXRofKafi4ErjzjfjQu4UYO5w0zpvlK8Os/edit?usp=sharing
+"
+
+# Daily_Sub-basin-wise_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned
+g_drive_Sub-basin = "https://docs.google.com/spreadsheets/d/1XKxrZeUBNWBPfI-bt21ubmql1w3ha4LfTL7znQ4Uh8s/edit?usp=sharing
+"
+
+# Daily_data_of_reservoir_level_of_CWC_Agency_2000_2023
+g_drive_CWC_agency = "https://docs.google.com/spreadsheets/d/1NK3pVebnRxcKzmtAn-WkMerZP_OKO9GaZFOHIR2Ro0M/edit?usp=sharing"
+
+# Read the CSV file from Google Drive
+#df = pd.read_csv(google_drive_link)
+
 
 # Load CSV data into DataFrames
-df_rainfall_state = pd.read_csv(rainfall_state_url)
+df_rainfall_state = pd.read_csv(g_drive_rainfall)
 df_rainfall_state['Date'] = pd.to_datetime(df_rainfall_state['Date'])
 
-df_cleaned = pd.read_csv(cleaned_url)
+df_cleaned = pd.read_csv(g_drive_Sub-basin)
 df_cleaned['Date'] = pd.to_datetime(df_cleaned['Date'])
 
 df_surface = pd.read_csv(surface_url)
-df_reservoirs = pd.read_csv(reservoirs_url)
+df_reservoirs = pd.read_csv(g_drive_CWC_agency)
 
+
+
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------
 # # Load your CSV data into a DataFrame
 # df_rainfall_state = pd.read_csv('Daily_Rainfall_data_from_IMD_and_NRSC_2018_2023_Cleaned.csv')
 
